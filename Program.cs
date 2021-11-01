2f29c426b4239cda4e4c1a3cd1435e22d9f8141a
iftekhar <83159641+Sarder-Iftekhar@users.noreply.github.com>
2021-11-02 03:40:33 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = val1 + val2;
+            Console.WriteLine(sum);
+        }
+    }
+}

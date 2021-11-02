987ef59715bbf37312ea7f212e95d08f05f4c44a
iftekhar <83159641+Sarder-Iftekhar@users.noreply.github.com>
2021-11-02 19:11:02 +0600

    Add files via upload

---
+++ lonely_integer.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int s=0;
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        count=0;
+        for(int j=0;j<n;j++)
+        {
+            if(a[i]==a[j])
+                count++;
+        }
+        if(count==1)
+        {
+            s=a[i];
+            break;
+
+
+        }
+    }
+    cout<<s;
+
+    return 0;
+
+
+}

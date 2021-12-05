1badc07b94457dff545bf877ad4fd13e8a024725
iftekhar <83159641+Sarder-Iftekhar@users.noreply.github.com>
2021-12-06 02:57:49 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace addition
     {
         static void Main(string[] args)
         {
-            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val = Convert.ToInt32(Console.ReadLine());
             int val2 = Convert.ToInt32(Console.ReadLine());
-            int sum = val1 + val2;
+            int sum = val + val2;
             Console.WriteLine(sum);
         }
     }
